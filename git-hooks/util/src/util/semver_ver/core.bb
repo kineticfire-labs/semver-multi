b1ed7fae0a6a91de3bd75f236b3712a6156ec5ed
Kris Hall <kjh@ieee.org>
2024-04-09 07:37:02 -0400

    more(p.h.u.v): start semver-ver utility

---
+++ git-hooks/util/src/util/semver_ver/core.bb
@@ -0,0 +1,52 @@
+#!/usr/bin/env bb
+
+;; (c) Copyright 2023-2024 KineticFire. All rights reserved.
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;     http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+
+;; KineticFire Labs
+;;	  Project site:  https://github.com/kineticfire-labs/semver-multi
+
+
+
+(ns util.semver-ver.core
+  (:require [clojure.string    :as str]
+            [babashka.process  :refer [shell]]
+            [clojure.java.io   :as io]
+            [cheshire.core     :as json]
+            [common.core       :as common]))
+
+
+
+;; version updated by CI pipeline
+(def ^:const version "latest")
+
+
+
+;; Implemented 'main' functionality here for testability due to constants
+(defn ^:impure perform-main
+  ""
+  [cli-args]
+  )
+
+
+(defn ^:impure -main
+  ""
+  [& args]
+  (perform-main args))
+
+
+;; execute 'main' function if run as a script, but don't execute 'main' if just loading the script
+(when (= *file* (System/getProperty "babashka.file"))
+  (apply -main *command-line-args*))
---
+++ git-hooks/util/test/util/semver_ver/core_test.clj
@@ -0,0 +1,76 @@
+;; (c) Copyright 2023-2024 KineticFire. All rights reserved.
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;     http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+
+;; KineticFire Labs
+;;	  Project site:  https://github.com/kineticfire-labs/semver-multi
+
+
+(ns util.semver-ver.core-test
+  (:require [clojure.test                      :refer [deftest is testing]]
+            [babashka.classpath                :as cp]
+            [babashka.process                  :refer [shell]]
+            [clojure.java.io                   :as io]
+            [util.semver-ver.core              :as v]
+            [common.core                       :as common])
+  (:import (java.io File)))
+
+
+(cp/add-classpath "./")
+
+
+(def ^:const temp-dir-string "gen/test/core_test")
+
+(def ^:const resources-test-data-dir-string "test/resources/data")
+
+
+
+;; from https://clojuredocs.org/clojure.core/with-out-str#example-590664dde4b01f4add58fe9f
+(defmacro with-out-str-data-map
+  "Performs the form in the `body` and returns a map result with key 'result' set to the return value and 'str' set to the string output, if any.  Equivalent to 'with-out-str' but returns a map result."
+  [& body]
+  `(let [s# (new java.io.StringWriter)]
+     (binding [*out* s#]
+       (let [r# ~@body]
+         {:result r#
+          :str    (str s#)}))))
+
+
+(defn delete-dir
+  "Deletes the file or directory `file`.  If `file` is a directory, then first recursively deletes all contents."
+  [^File file]
+  (when (.isDirectory file)
+    (run! delete-dir (.listFiles file)))
+  (io/delete-file file))
+
+
+(defn setup-temp-dir
+  "Sets up the temporary directory for the tests in this file.  Creates the directory if it does not exists, recursively deleting the directory first if it does exist."
+  []
+  (let [temp-dir (File. temp-dir-string)]
+    (when (.exists temp-dir)
+      (delete-dir temp-dir))
+    (.mkdirs temp-dir)))
+
+
+;; Configures temporary directory for tests
+(setup-temp-dir)
+
+
+
+(comment (deftest get-highlight-code-test
+  (testing "true"
+    (is (= common/shell-color-red (d/get-highlight-code true))))
+  (testing "false"
+    (is (= common/shell-color-white (d/get-highlight-code false))))))
